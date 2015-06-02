f1ddec856628c437bc8e5b3723705ef9224408c7
tudor <tudor.mazilu@pure360.com>
2015-06-03 01:17:30 +0300

    Made the required changes for point 3.

--- src/hiveHomework.sh
+++ src/hiveHomework.sh
@@ -8,12 +8,12 @@ IP=$2
 echo IP="$IP"
 
 if [ -n "$OPTION" ]; then
-  if [[ $OPTION != "1" &&  $OPTION != "2" ]]; then
-    echo "Please specify a valid option: 1 or 2"
+  if [[ $OPTION -lt 1 ||  $OPTION -gt 4 ]]; then
+    echo "Please specify a valid option: 1, 2, 3 or 4"
     exit 1
   fi
 else
-  echo "Please specify what option you want: 1 or 2"
+  echo "Please specify what option you want: 1, 2, 3 or 4"
   exit 1
 fi
 
@@ -28,9 +28,22 @@ if [ -n "$IP" ]; then
 
   beeline -u $USER -n admin -d $DRIVER -f filteredStats.sql -hiveconf totalLength=`cat /tmp/result/000000_0` -hiveconf tableNo=$OPTION -hiveconf ip=$IP
 else
-  beeline -u $USER -n admin -d $DRIVER -e "insert overwrite local directory '/tmp/result' SELECT CAST(SUM(size) as BIGINT) AS total_length FROM tokenized_access_logs$OPTION"
-
-  beeline -u $USER -n admin -d $DRIVER -f allStats.sql -hiveconf totalLength=`cat /tmp/result/000000_0` -hiveconf tableNo=$OPTION
+  case $OPTION in 
+    2) 
+      beeline -u $USER -n admin -d $DRIVER -e "insert overwrite local directory '/tmp/result' SELECT CAST(SUM(size) as BIGINT) AS total_length FROM tokenized_access_logs$OPTION"
+  
+      beeline -u $USER -n admin -d $DRIVER -f allStats.sql -hiveconf totalLength=`cat /tmp/result/000000_0` -hiveconf tableNo=$OPTION
+      ;;
+    3)
+      beeline -u $USER -n admin -d $DRIVER -f urlStats1.sql
+      ;;
+    4)
+      beeline -u $USER -n admin -d $DRIVER -f urlStats2.sql
+      ;;
+    *) 
+      echo "Invalid option"
+      ;;
+  esac
 fi
 
 
